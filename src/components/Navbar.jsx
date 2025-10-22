27416cfaef0d12de5013390d122ae91b0bcae169
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2025-10-22 11:51:59 +0200

    Create NavLayout with Navbar component and Outlet to wrap pages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+export function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/destination">Destination</Link>
+        </li>
+        <li>
+          <Link to="/crew">Crew</Link>
+        </li>
+        <li>
+          <Link to="/technology">Technology</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
