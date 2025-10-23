cc45470ea45ef135757642a1a20787f4bdb3ee69
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2025-10-23 11:01:23 +0200

    Apply background image to NavLayout

--- src/components/NavLayout/NavLayout.jsx
+++ src/components/NavLayout/NavLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../Navbar/Navbar";
 import styles from "./NavLayout.module.css";
-import bgImageHome from "/assets/home/background-home-desktop.jpg";
+import bgImageHome from "/assets/home/background-home-mobile.jpg";
 
 export function NavLayout() {
   return (
-    <div>
+    <div
+      className={styles.layout}
+      style={{ backgroundImage: `url(${bgImageHome})` }}
+    >
       <header>
         <Navbar />
       </header>
