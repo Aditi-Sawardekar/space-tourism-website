0eff9c8cacada0ca43fdca9fae055f6b9fe896cc
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2025-10-23 10:31:41 +0200

    Update NavLayout with semantic HTML: header for Navbar and main for Outlet

--- src/components/NavLayout/NavLayout.jsx
+++ src/components/NavLayout/NavLayout.jsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
-import { Navbar } from "./Navbar";
+import { Navbar } from "../Navbar/Navbar";
+import styles from "./NavLayout.module.css";
+import bgImageHome from "/assets/home/background-home-desktop.jpg";
 
 export function NavLayout() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <div>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </div>
   );
 }
